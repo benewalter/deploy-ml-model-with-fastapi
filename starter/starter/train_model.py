5536216216027de827593f1e1ec01894b87fcf67
benewalter <benewalter22@googlemail.com>
2023-06-24 22:13:42 +0200

    update train_model.py and create train_model.ipynb

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,14 @@
 # Script to train machine learning model.
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+data = pd.read_csv("/home/benewalter/deploy-ml-model-with-fastapi/starter/data/census.csv", skipinitialspace=True)
+
+print(data.head())
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -24,5 +28,8 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", encoder= encoder, lb = lb,training=False
+)
 
 # Train and save a model.
