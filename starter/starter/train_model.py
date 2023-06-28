2b2c4993865a209ea3216a31a273fbdb73031830
benewalter <benewalter22@googlemail.com>
2023-06-28 15:37:07 +0200

    add logging to train_model.py

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -5,15 +5,21 @@ from ml.data import process_data
 from ml.model import train_model, compute_model_metrics, inference
 import joblib
 import os
+import logging
+
 
 # Add the necessary imports for the starter code.
 
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
 # Add code to load in the data.
+logging.info('Loading data')
 data = pd.read_csv(os.getcwd()[:-8] + "/data/census.csv", skipinitialspace=True)
 
 #print(data.head())
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
+logging.info('Splitting data into train and test set')
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -28,11 +34,13 @@ cat_features = [
 ]
 
 # Process the train data with the process_data function.
+logging.info('Processing train data')
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Process the test data with the process_data function.
+logging.info('Processing test data')
 X_test, y_test, _, _ = process_data(
     test, categorical_features=cat_features, label="salary", 
     encoder= encoder, lb = lb,training=False
@@ -41,17 +49,21 @@ X_test, y_test, _, _ = process_data(
 # Train and save a model.
 
 # Train logistic regression
+logging.info('Training machine learning model')
 lr_model = train_model(X_train, y_train)
 
 # Apply logistic regression to test set
+logging.info('Applying machine learning model')
 predictions = inference(lr_model, X_test)
 
 # Check results
+logging.info('Checking inference results')
 precision, recall, fbeta = compute_model_metrics(y_test, predictions)
 print("Precision: " + str(precision))
 print("Recall: " + str(recall))
 print("Fbeta: " + str(fbeta))
 
 # Save model
+logging.info('Saving machine learning model')
 model_path = os.getcwd()[:-8] + "/model/lr_model.joblib"
 joblib.dump(lr_model, model_path)
