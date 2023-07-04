8d01d43c585aef969fa4d7102eeb7c9225c90d58
benewalter <benewalter22@googlemail.com>
2023-07-04 23:53:55 +0200

    launch heroku app and test live post

---
+++ starter/post_to_heroku_app.py
@@ -0,0 +1,24 @@
+import requests
+
+data = {
+    "age": 34,
+    "workclass": "Private",
+    "fnlgt": 245487,
+    "education": "7th-8th",
+    "education-num": 4,
+    "marital-status": "Married-civ-spouse",
+    "occupation": "Transport-moving",
+    "relationship": "Husband",
+    "race": "Amer-Indian-Eskimo",
+    "sex": "Male",
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 45,
+    "native-country": "Mexico",
+}
+
+response = requests.post("https://udacity-salary-prediction-94c031866332.herokuapp.com/inference/", 
+                         data=json.dumps(data))
+
+print("Status Code: " + str(response.status_code))
+print("Response: " + str(response.json()))
